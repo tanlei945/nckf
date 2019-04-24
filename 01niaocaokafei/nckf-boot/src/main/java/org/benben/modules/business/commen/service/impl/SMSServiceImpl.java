b45c2c6093165391f62f6a06d95198c9cd82286d
wanghao <wanghao@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-04-24 10:22:03 +0000

    新增阿里云短信、腾讯云短信

--- 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/commen/service/impl/SMSServiceImpl.java
+++ 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/commen/service/impl/SMSServiceImpl.java
@@ -1,17 +1,28 @@
 package org.benben.modules.business.commen.service.impl;
 
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
+import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
+import com.github.qcloudsms.*;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.SimpleHttpConnectionManager;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpMethodParams;
 import org.apache.commons.lang.StringUtils;
-import org.benben.common.menu.SMSEnum;
+import org.benben.common.menu.SMSResultEnum;
+import org.benben.common.util.DateUtils;
 import org.benben.common.util.RedisUtil;
+import org.benben.common.util.aliyun.AliMessageSend;
 import org.benben.modules.business.commen.service.ISMSService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import com.github.qcloudsms.httpclient.HTTPException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: WangHao
@@ -19,12 +30,21 @@ import org.springframework.stereotype.Service;
  * @description: 短信业务层
  */
 @Service
+@Slf4j
 public class SMSServiceImpl implements ISMSService {
 
-    @Value("${sms-configure.host}")
+    @Value("${sms.host}")
     private String host;
-    @Value("${sms-configure.appcode}")
+    @Value("${sms.appcode}")
     private String appcode;
+    @Value("${tencentsms.appId}")
+    private int tencentAppId;
+    @Value("${tencentsms.appkey}")
+    private String tencentAppkey;
+    @Value("${tencentsms.templateId}")
+    private int tencentTemplateId;
+    @Value("${tencentsms.smsSign}")
+    private String tencentSmsSign;
 
     @Autowired
     private RedisUtil redisUtil;
@@ -76,13 +96,48 @@ public class SMSServiceImpl implements ISMSService {
             ((SimpleHttpConnectionManager) httpclient.getHttpConnectionManager()).shutdown();
         }
 
-        if (StringUtils.equals(returncode, SMSEnum.SEND_SUCCESS.getCode())) {
+        if (StringUtils.equals(returncode, SMSResultEnum.SEND_SUCCESS.getCode())) {
             //存入redis
             redisUtil.set(mobile + "," + event, String.valueOf(code), 300);
         }
         System.out.println("验证码:" + code);
 
         return returncode;
+    }
+
+    /**
+     * 短信发送公用模板
+     * @param mobile
+     * @param event
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public SendSmsResponse aliSend(String mobile, String event) {
+
+        log.error("请求短信serve" + DateUtils.now());
+
+        SendSmsResponse sendSmsResponse = new SendSmsResponse();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        if (StringUtils.isNotBlank(event)) {   //发起短信验证
+
+            Integer code = (int) ((Math.random() * 9 + 1) * 100000);
+
+            map.put("code", code);
+
+//            log.error("调用redis,Set值"+System.currentTimeMillis());
+            log.error("调用redis,Set值" + mobile + event + "code: " + code.toString() + "  时间" + System.currentTimeMillis());
+            redisUtil.set(mobile + "," + event, String.valueOf(code), 300);
+
+//            log.error("短信服务调起时间"+System.currentTimeMillis());
+            sendSmsResponse = AliMessageSend.sendSms(mobile, JSONObject.toJSONString(map), "SMS_139570048");
+//            log.error("手机验证码"+sendSmsResponse);
+//            log.error("短信服务响应时间"+System.currentTimeMillis());
+//            log.error("*************结束*************");
+        }
+        return sendSmsResponse;
     }
 
     /**
@@ -112,4 +167,142 @@ public class SMSServiceImpl implements ISMSService {
 
         return num;
     }
+
+    /**
+     * 自定义短信内容发送
+     * @param msg 短信内容
+     * @param mobile 用户手机号
+     * @return
+     */
+    @Override
+    public String tencentSendMess(String msg, String mobile) {
+        try {
+            SmsSingleSender ssender = new SmsSingleSender(tencentAppId, tencentAppkey);
+            SmsSingleSenderResult result = ssender.send(0, "86", mobile,
+                    msg, "", "");
+            System.out.print(result);
+            return result.errMsg;
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 指定模板id发送短信
+     * @param mobile
+     * @return
+     */
+    @Override
+    public String tencentSendMesModel(String mobile) {
+        try {
+            String[] params = {"hello" , "1" };//{参数}
+            SmsSingleSender ssender = new SmsSingleSender(tencentAppId, tencentAppkey);
+            SmsSingleSenderResult result = ssender.sendWithParam("86", mobile,
+                    tencentTemplateId, params, tencentSmsSign, "", "");  // 签名参数未提供或者为空时，会使用默认签名发送短信
+            System.out.print(result);
+            return result.errMsg;//OK
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    /**
+     * 群发自定义短信
+     * @param msg
+     * @param mobile
+     * @return
+     */
+    @Override
+    public String tencentSendMesModel(String msg, String[] mobile) {
+        try {
+            SmsSingleSender ssender = new SmsSingleSender(tencentAppId, tencentAppkey);
+            SmsSingleSenderResult result = ssender.send(0, "86", String.valueOf(mobile),
+                    msg, "", "");
+            System.out.print(result);
+            return result.errMsg;
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    /**
+     * 指定模板ID群发
+     * @param mobile
+     * @return
+     */
+    @Override
+    public String tencentSendMesModel(String[] mobile) {
+
+        try {
+            String[] params = {"5678"};
+            SmsMultiSender msender = new SmsMultiSender(tencentAppId, tencentAppkey);
+            SmsMultiSenderResult result =  msender.sendWithParam("86", mobile,
+                    tencentTemplateId, params, tencentSmsSign, "", "");  // 签名参数未提供或者为空时，会使用默认签名发送短信
+            System.out.println(result);
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 发送语音消息
+     * @param msg
+     * @param mobile
+     * @return
+     */
+    @Override
+    public String sendMesVoice(String msg, String mobile) {
+        try {
+            SmsVoiceVerifyCodeSender vvcsender = new SmsVoiceVerifyCodeSender(tencentAppId,tencentAppkey);
+            SmsVoiceVerifyCodeSenderResult result = vvcsender.send("86",mobile,
+                    msg, 2, "");
+            System.out.print(result);
+            return result.errMsg;
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
