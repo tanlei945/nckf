9265018cb99b5b1da16c4f0a62e1eb2ba18b3d95
haowenxiang <haowenxiang@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-04-28 09:50:17 +0000

    当前送货地点接口

---
+++ src/main/java/org/benben/modules/business/rideraddress/service/impl/RiderAddressServiceImpl.java
@@ -0,0 +1,19 @@
+package org.benben.modules.business.rideraddress.service.impl;
+
+import org.benben.modules.business.rideraddress.entity.RiderAddress;
+import org.benben.modules.business.rideraddress.mapper.RiderAddressMapper;
+import org.benben.modules.business.rideraddress.service.IRiderAddressService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 骑手位置表
+ * @author： jeecg-boot
+ * @date：   2019-04-27
+ * @version： V1.0
+ */
+@Service
+public class RiderAddressServiceImpl extends ServiceImpl<RiderAddressMapper, RiderAddress> implements IRiderAddressService {
+
+}
