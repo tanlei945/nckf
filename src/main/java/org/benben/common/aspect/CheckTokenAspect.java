7ab67898df74731fc0d414c4c282d9a8c5cc841b
zhaoyonggang <zhaoyonggang@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-05-20 10:21:59 +0000

    检查token注解

---
+++ src/main/java/org/benben/common/aspect/CheckTokenAspect.java
@@ -0,0 +1,41 @@
+package org.benben.common.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.benben.common.api.vo.RestResponseBean;
+import org.benben.common.menu.ResultEnum;
+import org.benben.modules.business.user.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: YonggangZhao
+ * @Description:
+ * @Date: Created in 18:03 2019/5/20
+ * @Modified By:
+ */
+@Aspect
+@Component
+@Slf4j
+public class CheckTokenAspect {
+
+    @Pointcut("@annotation(org.benben.common.aspect.annotation.CheckToken)")
+    public void checkToken() {
+
+    }
+
+    @Around("checkToken()")
+    public RestResponseBean check(ProceedingJoinPoint point) throws Throwable {
+        User user = (User) SecurityUtils.getSubject().getPrincipal();
+        if(user==null) {
+            return new RestResponseBean(ResultEnum.TOKEN_OVERDUE.getValue(),ResultEnum.TOKEN_OVERDUE.getDesc(),null);
+        }
+        RestResponseBean restResponseBean = (RestResponseBean)point.proceed();
+        log.info("这是切面类------------的restResponseBean"+restResponseBean);
+        return  restResponseBean;
+    }
+}
