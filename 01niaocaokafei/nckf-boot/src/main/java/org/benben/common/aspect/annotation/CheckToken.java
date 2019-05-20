0a5e8e40776c686f06eec194cd2df98381ef71c3
zhaoyonggang <zhaoyonggang@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-05-20 10:21:47 +0000

    检查token注解

---
+++ 01niaocaokafei/nckf-boot/src/main/java/org/benben/common/aspect/annotation/CheckToken.java
@@ -0,0 +1,16 @@
+package org.benben.common.aspect.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @author: YonggangZhao
+ * @Description:
+ * @Date: Created in 18:02 2019/5/20
+ * @Modified By:
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface CheckToken {
+
+}
