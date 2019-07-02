e4a76db374a69b6f49f14c07b71553f376c387e4
tanlei <tanlei@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-07-02 09:06:51 +0000

    使用向导

---
+++ src/main/java/org/benben/modules/business/config/mapper/ConfigMapper.java
@@ -0,0 +1,17 @@
+package org.benben.modules.business.config.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.benben.modules.business.config.entity.Config;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 功能向导
+ * @author： jeecg-boot
+ * @date：   2019-07-02
+ * @version： V1.0
+ */
+public interface ConfigMapper extends BaseMapper<Config> {
+
+}
