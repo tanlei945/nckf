de90f344db7036ccadff93bd1beb0732f6028ab2
tanlei <tanlei@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-07-02 10:06:39 +0000

    版本/v1

--- 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/goods/GoodsSpec.java
+++ 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/goods/GoodsSpec.java
@@ -3,11 +3,11 @@ package org.benben.modules.business.goods;
 import lombok.Data;
 import org.benben.modules.business.goods.entity.Goods;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 
 @Data
 public class GoodsSpec {
     private Goods goods;
-    private Map<String, List<String>> map;
+    private Map<String, ArrayList<String>> map;
 }
