22f8f3e5353264f2fdb83e4b47e74230bc4567ac
tanlei <tanlei@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-07-03 10:44:20 +0000

    版本/v1

---
+++ 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/region/service/impl/RegionServiceImpl.java
@@ -0,0 +1,19 @@
+package org.benben.modules.business.region.service.impl;
+
+import org.benben.modules.business.region.entity.Region;
+import org.benben.modules.business.region.mapper.RegionMapper;
+import org.benben.modules.business.region.service.IRegionService;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+/**
+ * @Description: 城市列表
+ * @author： jeecg-boot
+ * @date：   2019-07-03
+ * @version： V1.0
+ */
+@Service
+public class RegionServiceImpl extends ServiceImpl<RegionMapper, Region> implements IRegionService {
+
+}
