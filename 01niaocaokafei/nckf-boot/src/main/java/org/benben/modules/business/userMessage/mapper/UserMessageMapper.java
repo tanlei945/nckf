a014545d6999c7ee444b80af3928554ea7caece6
tanlei <tanlei@31a602a2-70d2-49ef-bb5e-4fe84f325a75>
2019-07-06 03:06:55 +0000

    版本/v1

---
+++ 01niaocaokafei/nckf-boot/src/main/java/org/benben/modules/business/userMessage/mapper/UserMessageMapper.java
@@ -0,0 +1,17 @@
+package org.benben.modules.business.userMessage.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.benben.modules.business.userMessage.entity.UserMessage;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @Description: 用户消息表
+ * @author： jeecg-boot
+ * @date：   2019-07-04
+ * @version： V1.0
+ */
+public interface UserMessageMapper extends BaseMapper<UserMessage> {
+
+}
